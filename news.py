9733030799f19f55007aa95ab1ab170ad44d27e6
Siddharth Kumar <31590024+sidkum@users.noreply.github.com>
2017-10-16 18:45:35 +0530

    Update news.py

--- news.py
+++ news.py
@@ -93,27 +93,16 @@ def createResponse(speech, displayText,imageUrl):
 		"template_type":"generic",
 		"elements":[
 		   {
-		    "title":"Welcome to Peter\'s Hats",
-		    "image_url":"https://petersfancybrownhats.com/company_image.png",
-		    "subtitle":"We\'ve got the right hat for everyone.",
+		    "title":"today's news",
+		    "image_url":imageUrl,
+		    "subtitle":"subtitle of news.",
 		    "default_action": {
 		      "type": "web_url",
 		      "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
 		      "messenger_extensions": true,
 		      "webview_height_ratio": "tall",
 		      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-		    },
-		    "buttons":[
-		      {
-			"type":"web_url",
-			"url":"https://petersfancybrownhats.com",
-			"title":"View Website"
-		      },{
-			"type":"postback",
-			"title":"Start Chatting",
-			"payload":"DEVELOPER_DEFINED_PAYLOAD"
-		      }              
-		    ]      
+		    }   
 		  }
 		]
 	      }
