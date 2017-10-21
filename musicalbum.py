e18baf7838cd79a4c0e446640576b63e0229d1a4
Siddharth Kumar <31590024+sidkum@users.noreply.github.com>
2017-10-21 17:01:41 +0530

    Create musicalbum.py

---
+++ musicalbum.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "search.album":
+        speech = "Invalid Action specified"
+        return createResponse(speech, speech,data)
+    yql_url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=c68de8a6159c02cd683804aa40debc53&artist=adele&album=21&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    #return {
+#	"speech":data.get("status"),
+#	"displayText":data.get("source")
+ #   	}
+    res = makeWebhookResult(data)
+    return res
+
+
+
+
+def makeWebhookResult(data):
+    query = data.get("toptracks")
+    if query is None:
+        speech = "query element missing from music response"
+        return createResponse(speech, speech,data)
+    #if (title is None) or (description is None):
+    #    speech = "Hmm! Looks like we could not fetch the news"
+   # else:
+    #speech=" "
+    speech=data.get("toptracks").get("track")[0].get("image")[3].get("#text")
+    #for i in range(0,9):
+        #songname=data.get("toptracks").get("track")[i].get("name")
+        #speech=speech+songname+"\n"
+    # print(json.dumps(item, indent=4))
+
+##    print("speech=")
+##    print(speech)
+##    print("---------------")
+##    print(createResponse(speech, speech))
+##    print("------XXXX-----")
+
+    return createResponse(speech, speech,data)
+
+def createResponse(speech, displayText,data):
+##    print("Response:")
+##    print (speech)
+    topsongs=data.get("toptracks").get("track")
+    return {"speech":speech,
+	    "displayText":displayText,
+	    "data": {
+             "facebook": {
+             "attachment": {
+	    "type":"template",
+            "payload":{
+             "template_type":"generic",
+            "elements":[
+            {
+             "title":topsongs[0].get("name"),
+             "image_url":topsongs[0].get("image")[3].get("#text"),
+             "subtitle":"views:"+topsongs[0].get("playcount"),
+	     "default_action": {
+              "type": "web_url",
+              "url": topsongs[0].get("url"),
+             },
+	    "buttons":[
+              {
+                "type":"web_url",
+                "url":topsongs[0].get("url"),
+                "title":"View"
+              }
+	    ]
+           }
+	  ]
+        }}
+      }}
+   }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
