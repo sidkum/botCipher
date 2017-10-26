0d18eff83d1555da6ea745bd10476d07aff6b037
Siddharth Kumar <31590024+sidkum@users.noreply.github.com>
2017-10-26 11:19:49 +0530

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
 import http.client, urllib.parse, json
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+return r
 
 # **********************************************
 # *** Update or verify the following values. ***
