4cbbeb7095f818c1131cb432fb260c320315971a
Siddharth Kumar <31590024+sidkum@users.noreply.github.com>
2017-12-03 23:59:38 +0530

    Update musicalbum.py

--- musicalbum.py
+++ musicalbum.py
@@ -79,7 +79,7 @@ def makeWebhookResult(data):
 def createResponse(speech, displayText,data):
 ##    print("Response:")
 ##    print (speech)
- #   topsongs=data.get("album").get("tracks").get("track")[0].get("name")
+    topsongs=data.get("album").get("tracks").get("track")
          return {"speech":speech,
  	    "displayText":displayText,
 	    "data": {
@@ -92,7 +92,7 @@ def createResponse(speech, displayText,data):
                 {
                   "title": "Rolling in the Deep",
                   "image_url": data.get("album").get("image")[5].get("#text"),
-                  "subtitle": "views:12505022",
+                  "subtitle": topsongs[0].get("name") + "\n" +"topsongs[1].get("name"),
                   "default_action": {
                     "type": "web_url",
                     "url": "https://www.last.fm/music/Adele/_/Rolling+in+the+Deep"
